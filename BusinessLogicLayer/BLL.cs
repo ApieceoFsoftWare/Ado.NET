6c256ea5cf7155183c69ed9398052f9e8eac8d13
Ozan Berkay ÖZCAN <ozanberkay34@gmail.com>
2023-11-02 13:56:40 +0300

    DataAccessLayer - BusinessLogicLayer Codding 2

--- BusinessLogicLayer/BLL.cs
+++ BusinessLogicLayer/BLL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,23 @@ namespace BusinessLogicLayer
         {
             dll = new DatabaseLogicLayer.DLL();
         }
+        public int SistemKontrol(string kullaniciAdi, string sifre)
+        {
+            if(!string.IsNullOrEmpty(kullaniciAdi)&& !string.IsNullOrEmpty(sifre))
+            {
+                return dll.SistemKontrol(new Entities.Kullanici()
+                {
+                    KullaniciAdi = kullaniciAdi,
+                    Sifre = sifre
+                });
+            }
+            else
+            {
+                
+                return -1;
+            }
+        }
+
         public int KayitEkle(string isim, string soyisim, string TelefonNumarasiI, string TelefonNumarasiII, string TelefonNumarasiIII,
                              string EmailAdres, string WebAdres, string Adres, string Aciklama)
         {
@@ -37,5 +56,116 @@ namespace BusinessLogicLayer
             return -1; // Eksik parametre hatası
             }    
         }
+
+        public int KayitDuzenle(Guid ID,string isim, string soyisim, string TelefonNumarasiI, string TelefonNumarasiII, string TelefonNumarasiIII,
+                             string EmailAdres, string WebAdres, string Adres, string Aciklama)
+        {
+            if (ID != Guid.Empty)
+            {
+                return dll.KayitDuzenle(new Entities.Rehber()
+                {
+                    ID= ID,
+                    Isim= isim,
+                    Soyisim= soyisim,
+                    TelefonNumarasiI= TelefonNumarasiI,
+                    TelefonNumarasiII= TelefonNumarasiII,
+                    TelefonNumarasiIII= TelefonNumarasiIII,
+                    EmailAdres= EmailAdres,
+                    WebAdres= WebAdres,
+                    Adres= Adres,
+                    Aciklama= Aciklama
+                });
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int KayitSil(Guid ID)
+        {
+            if(ID != Guid.Empty)
+            {
+                return dll.KayitSil(ID);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public List<Rehber> KayitListele()
+        {
+            List<Rehber> RehberListesi = new List<Rehber>();
+
+            try
+            {
+                SqlDataReader reader = dll.KayitListele();
+                while (reader.Read())
+                {
+                    RehberListesi.Add(new Rehber()
+                    {
+                        ID = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),           // Buradaki isDbNull parametresi önemlidir.
+                        Isim = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),     // Eğer bunu kontrol etmezsek ve veritabanında ilgili row
+                        Soyisim = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),  // NOT NULL'sa hata alırız...
+                        TelefonNumarasiI = reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
+                        TelefonNumarasiII = reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
+                        TelefonNumarasiIII = reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
+                        EmailAdres = reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
+                        WebAdres = reader.IsDBNull(7) ? string.Empty : reader.GetString(7),
+                        Adres = reader.IsDBNull(8) ? string.Empty : reader.GetString(8),
+                        Aciklama = reader.IsDBNull(9) ? string.Empty : reader.GetString(9),
+                    });
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                dll.BaglantiAyarla();
+            }
+            return RehberListesi;
+        }
+        public Rehber KayitListeleID(Guid ID)
+        {
+            Rehber RehberKayit = new Rehber();
+
+            try
+            {
+                SqlDataReader reader = dll.KayitListeleID(ID);
+                while (reader.Read())
+                {
+                    RehberKayit = new Rehber()
+                    {
+                        ID = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),           // Buradaki isDbNull parametresi önemlidir.
+                        Isim = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),     // Eğer bunu kontrol etmezsek ve veritabanında ilgili row
+                        Soyisim = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),  // NOT NULL'sa hata alırız...
+                        TelefonNumarasiI = reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
+                        TelefonNumarasiII = reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
+                        TelefonNumarasiIII = reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
+                        EmailAdres = reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
+                        WebAdres = reader.IsDBNull(7) ? string.Empty : reader.GetString(7),
+                        Adres = reader.IsDBNull(8) ? string.Empty : reader.GetString(8),
+                        Aciklama = reader.IsDBNull(9) ? string.Empty : reader.GetString(9),
+                    };
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                dll.BaglantiAyarla();
+            }
+            return RehberKayit;
+        }
+
+
+
     }
 }
--- DatabaseLogicLayer/DLL.cs
+++ DatabaseLogicLayer/DLL.cs
@@ -32,6 +32,25 @@ namespace DatabaseLogicLayer
                 _conn.Close();
             }
         }
+        public int SistemKontrol(Kullanici kullanici)
+        {
+            try
+            {
+                _command = new SqlCommand("SELECT Count(*) FROM Kullanici WHERE KullaniciAdi = @KullaniciAdi AND Sifre = @Sifre", _conn);
+                BaglantiAyarla();
+                ReturnValues = (int)_command.ExecuteScalar();
+
+            }
+            catch(Exception ex) 
+            {
+
+            }
+            finally
+            {
+                BaglantiAyarla();
+            }
+            return ReturnValues;
+        }
         public int KayitEkle(Rehber rehber)
         {
             try
@@ -60,6 +79,80 @@ namespace DatabaseLogicLayer
             }
             finally { BaglantiAyarla(); }
             return ReturnValues;
+        }
+        public int KayitDuzenle(Rehber rehber)
+        {
+            try
+            {
+                _command = new SqlCommand(@"UPDATE Rehber 
+                                            Isim = @Isim,
+                                            Soyisim = @Soyisim,
+                                            TelefonNumarasiI = @TelefonNumarasiI,
+                                            TelefonNumarasiI = @TelefonNumarasiII,
+                                            TelefonNumarasiI = @TelefonNumarasiIII,
+                                            EmailAdres = @EmailAdres,
+                                            Adres = @Adres,
+                                            Aciklama = @Aciklama
+                                            WHERE ID = @ID
+                                        ",_conn);
+                _command.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = rehber.ID;
+                _command.Parameters.Add("@Isım", SqlDbType.NVarChar).Value = rehber.Isim;
+                _command.Parameters.Add("@Soyisim", SqlDbType.NVarChar).Value = rehber.Soyisim;
+                _command.Parameters.Add("@TelefonNumarasiI", SqlDbType.NVarChar).Value = rehber.TelefonNumarasiI;
+                _command.Parameters.Add("@TelefonNumarasiII", SqlDbType.NVarChar).Value = rehber.TelefonNumarasiII;
+                _command.Parameters.Add("@TelefonNumarasiIII", SqlDbType.NVarChar).Value = rehber.TelefonNumarasiIII;
+                _command.Parameters.Add("@EmailAdres", SqlDbType.NVarChar).Value = rehber.EmailAdres;
+                _command.Parameters.Add("@WebAdres", SqlDbType.NVarChar).Value = rehber.WebAdres;
+                _command.Parameters.Add("@Adres", SqlDbType.NVarChar).Value = rehber.Adres;
+                _command.Parameters.Add("@Aciklama", SqlDbType.NVarChar).Value = rehber.Aciklama;
+
+                BaglantiAyarla();               // Insert cümlemizi ayarladıktan sonra bu şekilde database ile olan bağlantıyı açacağız...
+
+                ReturnValues = _command.ExecuteNonQuery();     // Hazırladığımız SQL Sorgusunu SQL SERVER'a göndereceğiz...
+                                                               // Burada yazdığımız komut kaç tane row etkilendiyse
+                                                               // bunu bize interger olarak geri dönüyor.        
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            finally
+            {
+                BaglantiAyarla();
+            }
+            return ReturnValues;
+        }
+        public int KayitSil(Guid Id)
+        {
+            try
+            {
+                _command = new SqlCommand(@"DELETE Rehber WHERE ID = @ID", _conn);
+                _command.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = Id;
+                
+                BaglantiAyarla();               // Insert cümlemizi ayarladıktan sonra bu şekilde database ile olan bağlantıyı açacağız...
+
+                ReturnValues = _command.ExecuteNonQuery();     // Hazırladığımız SQL Sorgusunu SQL SERVER'a göndereceğiz...
+                                                               // Burada yazdığımız komut kaç tane row etkilendiyse
+                                                               // bunu bize interger olarak geri dönüyor.        
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally { BaglantiAyarla(); }
+            return ReturnValues;
+        }
+        public SqlDataReader KayitListele()
+        {
+            _command = new SqlCommand("SELECT * FROM Rehber", _conn);
+            BaglantiAyarla();
+            return _command.ExecuteReader();
+        }
+        public SqlDataReader KayitListeleID(Guid id)
+        {
+            _command = new SqlCommand("SELECT * Rehber WHERE ID = @id", _conn);
+            BaglantiAyarla();
+            return _command.ExecuteReader();
         }
 
     }
